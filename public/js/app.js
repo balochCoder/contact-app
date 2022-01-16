0a30833c4736293b0d8fc16cccf7c94aa89eac41
Raheel Ahmed <balochcoder@pop-os.localdomain>
2022-01-16 16:03:18 +0500

    Basic Search Button

--- public/js/app.js
+++ public/js/app.js
@@ -10,10 +10,34 @@ document.querySelectorAll(".btn-delete").forEach((button) => {
     button.addEventListener("click", function (event) {
         event.preventDefault();
         if (confirm("Are you sure?")) {
-            let action = this.getAttribute('href')
-            let form = document.getElementById('form-delete')
-            form.setAttribute('action',action)
-            form.submit()
+            let action = this.getAttribute("href");
+            let form = document.getElementById("form-delete");
+            form.setAttribute("action", action);
+            form.submit();
         }
     });
 });
+
+document.getElementById("btn-clear").addEventListener("click", () => {
+    let input = document.getElementById("search"),
+        select = document.getElementById("filter_company_id");
+
+    input.value = "";
+    select.selectedIndex = 0;
+
+    window.location.href = window.location.href.split("?")[0];
+});
+
+const toggleClearButton = () => {
+    let query = location.search,
+        pattern = /[?&]search=/,
+        button = document.getElementById("btn-clear");
+
+    if (pattern.test(query)) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+};
+
+toggleClearButton();
